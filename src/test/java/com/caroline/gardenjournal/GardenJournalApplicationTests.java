8d32b050a8743c0278e7cc4fe7448a8fa4c53759
Caroline Enggren <caroline.enggren@gmail.com>
2026-05-20 14:14:59 +0200

    Initial Spring Boot setup

---
+++ src/test/java/com/caroline/gardenjournal/GardenJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caroline.gardenjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenJournalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
