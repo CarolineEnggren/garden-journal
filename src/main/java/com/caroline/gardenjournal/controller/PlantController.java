f0195146a305e77b6ca5a3a22c581dfdcf421371
Caroline Enggren <caroline.enggren@gmail.com>
2026-05-20 15:32:45 +0200

    Add POST endpoint for plants

--- src/main/java/com/caroline/gardenjournal/controller/PlantController.java
+++ src/main/java/com/caroline/gardenjournal/controller/PlantController.java
@@ -3,6 +3,8 @@ package com.caroline.gardenjournal.controller;
 import com.caroline.gardenjournal.entity.Plant;
 import com.caroline.gardenjournal.repository.PlantRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,5 +23,13 @@ public class PlantController {
     @GetMapping("/plants")
     public List<Plant> getAllPlants() {
         return plantRepository.findAll();
+    }
+
+    // Creates and saves a new plant in the database
+    @PostMapping("/plants")
+    public Plant createPlant(@RequestBody Plant plant) {
+
+        // Saves the plant object received from the request body
+        return plantRepository.save(plant);
     }
 }
